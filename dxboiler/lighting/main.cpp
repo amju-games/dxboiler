aff4eddeaadb52aa571bf22a20227642a6db9d1a
jason.e.colman <jason.e.colman@gmail.com>
2012-10-24 17:06:57 +0000

    Fix formatting

--- dxboiler/lighting/main.cpp
+++ dxboiler/lighting/main.cpp
@@ -19,19 +19,17 @@ int main(int argc, char **argv)
 
   if (!DX11::CreateWindow()) 
   {
-	ReportError("Failed to create window!?");
-	return 1;
+    ReportError("Failed to create window!?");
+    return 1;
   }
-
-  
 
   DX11Shader* shader = new LightingShader; 
   // You derive from DX11Shader to do app-specific stuff 
   // Subclasses have vars corresponding to uniform vars in shader
-  
+
   if (!shader->Load("lighting.fx"))
   {
-	return 1;
+    return 1;
   }
 
   DX11::UseShader(shader);
@@ -49,22 +47,22 @@ int main(int argc, char **argv)
 
   while (true)
   {
-	DX11::GetInput()->Update();
-	DX11::InitFrame(0, 0, 1); // clear colour
+    DX11::GetInput()->Update();
+    DX11::InitFrame(0, 0, 1); // clear colour
 
-	DX11::SetMatrixMode(DX11_MODELVIEW_MATRIX);
-	DX11::SetIdentity();
-	DX11::LookAt(0, 0, 10,  0, 0, 0,  0, 1, 0);
+    DX11::SetMatrixMode(DX11_MODELVIEW_MATRIX);
+    DX11::SetIdentity();
+    DX11::LookAt(0, 0, 10,  0, 0, 0,  0, 1, 0);
 
-	static float d = 0;
-	d += 0.1f;
-	DX11::RotateY(d);
-	DX11::Draw(&vb);
+    static float d = 0;
+    d += 0.1f;
+    DX11::RotateY(d);
+    DX11::Draw(&vb);
 
-	DX11::Translate(3, 0, 0);
-	DX11::Draw(&box);
+    DX11::Translate(3, 0, 0);
+    DX11::Draw(&box);
 
-	DX11::Flip();
+    DX11::Flip();
   }
 
   return 0;
