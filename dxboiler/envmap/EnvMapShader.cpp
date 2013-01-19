0ca7e89e2ccc45762574ed84843c241f6bb6e06a
jason.e.colman <jason.e.colman@gmail.com>
2013-01-19 18:13:14 +0000

    Added sphere mapping demo

---
+++ dxboiler/envmap/EnvMapShader.cpp
@@ -0,0 +1,17 @@
+ /*
+DX Boiler: boilerplate code for DX11 projects.
+This source code (c) Copyright Jason Colman 2012.
+*/
+
+#include "EnvMapShader.h"
+
+void EnvMapShader::UpdateConstantBuffer()
+{
+  float view[16];
+  DX11::GetMatrix(DX11_MODELVIEW_MATRIX, view);
+  float proj[16];
+  DX11::GetMatrix(DX11_PROJECTION_MATRIX, proj);
+
+  m_cb.mView = XMMatrixTranspose(view);
+  m_cb.mProjection = XMMatrixTranspose(proj);
+}
